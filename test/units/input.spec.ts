d6d548518282c2ceef2422073a43354482efc2ca
danielsss <better.sunjian@gmail.com>
2021-07-12 16:27:39 +0800

    update: test case

--- test/units/input.spec.ts
+++ test/units/input.spec.ts
@@ -1,6 +1,7 @@
 import Input from '../../src/input';
 import { expect } from 'chai';
 import * as path from 'path';
+import * as fs from 'fs';
 
 describe('Input Unit Test', function () {
   it('initializing Input error', done => {
@@ -12,11 +13,17 @@ describe('Input Unit Test', function () {
     }
   });
 
-  it('should return an instance of Input correctly', done => {
-    const input = new Input({ source: path.resolve('.') });
+  it('should return an instance of Input correctly and including all *.xmind files', done => {
+    const source = path.resolve('.')
+    const input = new Input({ source });
     const options = input.opts();
     expect(options.excludes).to.include('node_modules');
-    expect(input.getLoadedFiles()).to.be.an('array');
+    const files = input.getLoadedFiles();
+    expect(files).to.be.an('array');
+
+    for (const file of files) {
+      expect(fs.existsSync(path.join(source, file))).to.be.true;
+    }
     done();
   });
 });
