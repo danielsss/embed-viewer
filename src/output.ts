7982667594d73743752c0d96fa614059d729b739
danielsss <better.sunjian@gmail.com>
2021-07-17 00:52:25 +0800

    update: Index UI

--- src/output.ts
+++ src/output.ts
@@ -3,7 +3,6 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as Debug from 'debug';
 import * as utils from './utils';
-import * as _ from 'lodash';
 import * as chalk from 'chalk';
 
 import { TITLE, LOGO_ADDR } from './config';
@@ -53,7 +52,7 @@ class Output {
       const template = Handlebars.compile(originalTemplate);
       const fp = path.join(this.source, file);
       const base64 = fs.readFileSync(fp, {encoding: 'base64'});
-      const compiled = template({ title: this.title, base64, logo: LOGO_ADDR });
+      const compiled = template({ title: this.title, base64, logo: this.logo });
       const arr = file.includes('/') ? file.split('/') : [ file ];
       const fullname = arr[arr.length - 1];
       let name = undefined;
@@ -66,26 +65,23 @@ class Output {
       const page = path.join(this.target, `/${name}.html`);
       debug('create page %s', page);
       fs.writeFileSync(page, compiled);
-      console.info('page %s is created', chalk.green(page));
+      console.info('%s is created', chalk.green(page));
     }
   }
 
   public compileIndex() {
     const template = Handlebars.compile(fs.readFileSync(INDEX_TEMPLATE, 'utf-8'));
     const files = [];
-    debug('files:', this.files);
     for (const str of this.files) {
       files.push(utils.resolvePathString(str));
     }
-    const html = this.navigator(_.groupBy(files, file => file.dir.join('/')));
-    debug('navigator html string:', html);
     const compiled = template({
-      title: this.title, html,
-      git: json.homepage, logo: LOGO_ADDR,
+      title: this.title,
+      git: json.homepage, logo: this.logo,
       struct: JSON.stringify(this.struct)
     });
     fs.writeFileSync(path.join(this.options.target, '/index.html'), compiled);
-    console.info('page %s is created', chalk.green('index.html'));
+    console.info('%s is created', chalk.green('index.html'));
   }
 
   public createScript(exit: boolean = false) {
@@ -97,7 +93,7 @@ class Output {
     debug('prepare to create %s into destination %s', SCRIPT_TEMPLATE, dest);
     const finish = () => {
       debug('script %s created', dest);
-      console.info('script %s is created.', chalk.green(SCRIPT_TEMPLATE))
+      console.info('%s is created.', chalk.green(SCRIPT_TEMPLATE))
       exit && process.exit(0);
     }
     const error = err => {
@@ -110,33 +106,6 @@ class Output {
       .pipe(fs.createWriteStream(dest))
       .on('finish', finish)
       .on('error', error);
-  }
-
-  protected navigator(groupInfo): string {
-    let html = '';
-
-    const keys = Object.keys(groupInfo);
-    const folderStyle = 'uk-list';
-    const fileStyle = 'uk-list uk-list-disc uk-list-primary';
-    const ul = ['<ul class="{style}">', '</ul>'];
-    const li = ['<li>', '</li>']
-    const folder = '<span uk-icon="icon: folder" style="margin-right: 10px;"></span>'
-    for (const key of keys) {
-      html += ul[0].replace('{style}', folderStyle);
-      html += `${li[0]}${folder}${key}${li[1]}`;
-      if (groupInfo[key].length > 0) {
-        html += ul[0].replace('{style}', fileStyle);
-        for (const obj of groupInfo[key]) {
-          const href = obj.name.replace('.xmind', '.html');
-          const name = obj.name.split('.')[0];
-          html += `${li[0]}<a href="${href}">${name}</a>${li[1]}`;
-        }
-        html += ul[1];
-      }
-      html += ul[1];
-    }
-
-    return html;
   }
 }
 
